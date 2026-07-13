923bd5a99816d51e04d34a3c34b761c9447eb492
Getu Tesfaye <getutesfaye919@gmail.com>
2026-07-13 06:28:45 -0700

    exercise_01.py

---
+++ module-01-foundation/days/day_01/exercise_01.py
@@ -0,0 +1,8 @@
+# pwd: Find out the exact path of the current folder you are standing in
+# mkdir:coding_workspace # create new folder
+# cd_coding_workspace: # newly inside the folder
+# touch:named.txt,readme.md,presentation.ppt,module_01.pdf # blank files created
+# echo: "hello_world" > notes.txt # write into the file
+# cat: notes.txt # verify the text written
+# ls -1a: # lists
+# rm: notes.txt # remove the file
