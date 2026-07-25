11f09d5b2e30f5b3b55407a1ad47c290ba4201fc
Getu Tesfaye <getutesfaye919@gmail.com>
2026-07-25 09:19:07 +0300

    project1_SavingAccount.py

---
+++ module-01-foundation/project/project_addis-bank/SavingAccount.py
@@ -0,0 +1,15 @@
+from Account import Account
+
+class SavingAccount(Account):
+    def __init__(self, owner, account_number, balance = 0, interest_rate = 0.05):
+        super().__init__(owner, account_number, balance)
+        self.interest_rate = interest_rate
+    def add_interest(self):
+        interest = self.balance * self.interest_rate
+        self.deposit(interest)
+        self.transaction.append(f"added interest: {interest}") 
+        print(f"added interest: {interest}")
+
+if __name__ == "__main__":
+    saving = SavingAccount("Getu Tesfaye", 1000456787543, 500, 0.05)
+    saving.add_interest()
